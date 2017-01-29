fe18c7fa900416ff96bf037b5149c21b0b9c7776
Den <Den.vovk89@gmail.com>
2017-01-29 20:48:45 +0500

    initialization project
    git init for project

---
+++ Hospital_b.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Hospital_b" в коде и файле конфигурации.
+    public class Hospital_b : IHospital_b
+    {
+        public string DoWork()
+        {
+            return "Den, you are amazing, man!!";
+        }
+    }
+}
